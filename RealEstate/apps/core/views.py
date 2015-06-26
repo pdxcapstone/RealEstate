5683510e0a4ff34249de83cbc1b24fd4c8d404e1
Aaron Hampshire <aaron.hampshire@gmail.com>
2015-06-26 16:16:17 -0700

    login cut/paste w slight mod from http://goo.gl/wkT1KN

--- RealEstate/apps/core/views.py
+++ RealEstate/apps/core/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.views.generic import View
+from django.contrib.auth import authenticate, login
 
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'core/homebuyerHome.html', {})
+
+
+def login_user(request):
+    state = "Please log in below..."
+    username = password = ''
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+            else:
+                state = "Your account is not active, please contact the site admin."
+        else:
+            state = "Your username and/or password were incorrect."
+
+    return render_to_response('core/auth.html',{'state':state, 'username': username})
--- RealEstate/urls.py
+++ RealEstate/urls.py
@@ -20,4 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('RealEstate.apps.core.urls')),
+    url(r'^login/$', 'RealEstate.apps.core.views.login_user'),
 ]
