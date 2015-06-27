cdf00c674410babb7baff33ec4247681361bac13
Aaron Hampshire <aaron.hampshire@gmail.com>
2015-06-27 09:51:53 -0700

    Login redirects

--- RealEstate/apps/core/views.py
+++ RealEstate/apps/core/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
 from django import forms
 
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'core/homebuyerHome.html', {})
-
+        if request.user.is_authenticated():
+          return render(request, 'core/homebuyerHome.html', {})
+        return HttpResponseRedirect('/login/')
+        
 
 def login_view(request):
     state = (False, "")     # Tuple to store status (True if logged in) and message.
@@ -23,6 +26,7 @@ def login_view(request):
             if user.is_active:
                 login(request, user)
                 state = (True, "You're successfully logged in!")
+                return HttpResponseRedirect('/')
             else:
                 state = (False, "Your account is not active, please contact the site admin.")
         else:
@@ -32,7 +36,7 @@ def login_view(request):
         username = forms.CharField(label='Username', max_length=100)
         password = forms.CharField(label='Password', widget=forms.PasswordInput())
         
-    context = {"form" : LoginForm(), 'state':state, 'username': username}
+    context = {'form' : LoginForm(), 'state':state, 'username': username}
     return render(request, template, context)
 
 
