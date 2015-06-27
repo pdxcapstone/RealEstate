9b2705f2bf2bf5f30a935580aac4e38642a6030d
Aaron Hampshire <aaron.hampshire@gmail.com>
2015-06-27 11:54:03 -0700

    Small template refactor, begin adding simple queries

--- RealEstate/apps/core/views.py
+++ RealEstate/apps/core/views.py
@@ -3,12 +3,15 @@ from django.views.generic import View
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django import forms
+from .models import House, Homebuyer, Couple
 
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated():
-          return render(request, 'core/homebuyerHome.html', {})
+          couple = Couple.objects.filter(homebuyer__user=request.user)
+          house = House.objects.filter(couple=couple)
+          return render(request, 'core/homebuyerHome.html', {'couple': couple, 'house': house})
         return HttpResponseRedirect('/login/')
         
 
