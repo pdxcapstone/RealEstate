c84e20aab3eb006c9482a971431e5d05e5b64a86
Souriya Khaosanga <souriya.khaosanga@gmail.com>
2015-07-03 15:18:21 -0700

    adds stub for next model test

--- RealEstate/apps/pending/test_models.py
+++ RealEstate/apps/pending/test_models.py
@@ -1,1 +1,8 @@
-__author__ = 'sour'
+from django.utils import unittest
+
+class TestModelsPending(unittest.TestCase):
+    def test_pendingcouple(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
