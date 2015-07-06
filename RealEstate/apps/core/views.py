c235010e46f22dd8194da9bcb29145cdc1de6688
Aaron Hampshire <aaron.hampshire@gmail.com>
2015-07-06 06:40:04 -0700

    Functioning eval page with dropdown selects

--- RealEstate/apps/core/views.py
+++ RealEstate/apps/core/views.py
@@ -6,7 +6,7 @@ from django.utils.decorators import method_decorator
 from django.views.generic import View
 from django import forms
 
-from .models import Couple, House, Category, Grade
+from .models import Homebuyer, Couple, House, Category, Grade
 
 
 def login(request, *args, **kwargs):
@@ -41,8 +41,9 @@ class HomeView(BaseView):
 
 
 class EvalView(BaseView):
+
     """
-    View for the Home Evaluation Page. Currently, this page is decoupled 
+    View for the Home Evaluation Page. Currently, this page is decoupled
     from the rest of the app and uses static elements in the database.
     """
     def get(self, request, *args, **kwargs):
@@ -50,7 +51,43 @@ class EvalView(BaseView):
         categories = Category.objects.filter(couple=couple)
 
         # A specific house should be passed into the request, changing next two lines.
-        house = House.objects.filter(couple=couple) 
+        house = House.objects.filter(couple=couple)
+        grades = Grade.objects.filter(house=house[0])
+
+        # Merging grades and categories to provide object with both information.
+        # Data Structure: [(cat1, score1), (cat2, score2), ...]
+        graded = []
+        for category in categories:
+            missing = True
+            for grade in grades:
+                if grade.category.id is category.id:
+                    graded.append((category, grade.score))
+                    missing = False
+                    break
+            if missing:
+                graded.append((category, None))
+        class ContactForm(forms.Form):
+            def __init__(self, *args, **kwargs):
+                super(ContactForm, self).__init__(*args, **kwargs)
+                for c, s in graded:
+                    self.fields[c.summary] = forms.CharField(initial="0" if None else s, widget=forms.HiddenInput())
+
+        context = {'couple': couple, 'house' : house[0], 'grades': graded, "form" : ContactForm() }
+        return render(request, 'core/houseEval.html', context)
+
+    def post(self, request, *args, **kwargs):
+        homebuyer = Homebuyer.objects.filter(user_id=request.user.id)
+        couple = Couple.objects.filter(homebuyer__user=request.user)
+        categories = Category.objects.filter(couple=couple)
+
+        # A specific house should be passed into the request, changing next two lines.
+        house = House.objects.filter(couple=couple)
+
+        for category in categories:
+            value = request.POST.get(category.summary)
+            grade, created = Grade.objects.update_or_create(
+                homebuyer=homebuyer[0], category=category, house=house[0], defaults={'score': int(value)})
+
         grades = Grade.objects.filter(house=house[0])
 
         # Merging grades and categories to provide object with both information.
@@ -71,7 +108,7 @@ class EvalView(BaseView):
                 super(ContactForm, self).__init__(*args, **kwargs)
                 for c, s in graded:
                     self.fields[c.summary] = forms.CharField(initial="0" if None else s, widget=forms.HiddenInput())
-
+        
         context = {'couple': couple, 'house' : house[0], 'grades': graded, "form" : ContactForm() }
         return render(request, 'core/houseEval.html', context)
         
