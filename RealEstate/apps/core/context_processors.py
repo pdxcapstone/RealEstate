3a94695e6efbbf1d0aacdede27c94d7968fbed6a
Chris Schmitz <Christopher_Schmitz@mentor.com>
2015-08-15 15:49:52 -0700

    Move categories to home page button

--- RealEstate/apps/core/context_processors.py
+++ RealEstate/apps/core/context_processors.py
@@ -11,13 +11,3 @@ def async_login_form(request):
     if not request.user.is_authenticated():
         context['login_form'] = AuthenticationForm()
     return context
-
-
-def navbar(request):
-    navbar = {}
-    user = request.user
-    if user.is_authenticated():
-        role = user.role_object
-        if role and role.role_type == 'Homebuyer':
-            navbar['categories'] = True
-    return {'navbar': navbar}
