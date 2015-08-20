b30abe889b00da3217dde284ba7e91719315c032
Chris Schmitz <Christopher_Schmitz@mentor.com>
2015-08-19 22:13:13 -0700

    Fix migration weirdness

--- RealEstate/apps/core/migrations/0008_merge.py
+++ RealEstate/apps/core/migrations/0008_merge.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('core', '0007_auto_20150805_0714'),
-        ('core', '0003_auto_20150723_1355'),
+        ('core', '0003_auto_20150727_0426'),
     ]
 
     operations = [
